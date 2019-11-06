dd42f5e55b17b6e73344754dd1067d03d686287e
Zach Howard <obdo32@gmail.com>
2019-11-05 20:36:02 -0800

    another bit of learning

--- src/ai/mage.ts
+++ src/ai/mage.ts
@@ -3,7 +3,7 @@ import { BaseCharacter } from "lib/BaseCharacter";
 import { Party } from "lib/Party";
 
 greeting("TypeScript");
-map_key("1", "snippet", "parent.start_runner();");
+map_key("1", "sjnippet", "parent.start_runner();");
 map_key("2", "snippet", "parent.stop_runner();");
 map_key("3", "snippet", 'load_code("' + character.ctype + '")');
 game_log("To reload your code, first press 2 to stop the current AI, and then press 3 to reload the code.");
--- src/ai/priest.ts
+++ src/ai/priest.ts
@@ -6,7 +6,8 @@ map_key("2", "snippet", "parent.stop_runner();");
 map_key("3", "snippet", 'load_code("' + character.ctype + '")');
 game_log("To reload your code, first press 2 to stop the current AI, and then press 3 to reload the code.");
 
-var attack_mode=true
+var attack_mode=false;
+
 
 setInterval(function(){
 
